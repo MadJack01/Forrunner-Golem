6bc9a8e105d8547ae6894e3faddf4010663951d3
Alexis Migniau <migniau.alexis@gmail.com>
2026-01-31 21:18:16 +0100

    feat(world) : Commande de Changement de bloc

--- src/main/java/com/light06/plugin/ForerunnerGolem/Commands/BossUiTestCommand.java
+++ src/main/java/com/light06/plugin/ForerunnerGolem/Commands/BossUiTestCommand.java
@@ -1,24 +1,22 @@
 package com.light06.plugin.ForerunnerGolem.Commands;
 
+import com.hypixel.hytale.builtin.buildertools.BuilderToolsPlugin;
+import com.hypixel.hytale.builtin.buildertools.utils.Material;
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
-import com.hypixel.hytale.protocol.packets.interface_.NotificationStyle;
-import com.hypixel.hytale.server.core.Message;
+import com.hypixel.hytale.math.vector.Vector3i;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
 import com.hypixel.hytale.server.core.entity.entities.Player;
-import com.hypixel.hytale.server.core.entity.entities.player.hud.HudManager;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
+import com.hypixel.hytale.server.core.prefab.selection.standard.BlockSelection;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
-import com.hypixel.hytale.server.core.util.EventTitleUtil;
-import com.hypixel.hytale.server.core.util.NotificationUtil;
 import com.light06.plugin.ForerunnerGolem.Events.TriggerBossEvent;
-import com.light06.plugin.ForerunnerGolem.UI.BossHealthHud;
-import com.light06.plugin.ForerunnerGolem.UI.EmptyHudUI;
 
 import javax.annotation.Nonnull;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class BossUiTestCommand extends AbstractPlayerCommand {
     public BossUiTestCommand(@Nonnull String name, @Nonnull String description, boolean requiresConfirmation) {
@@ -27,33 +25,45 @@ public class BossUiTestCommand extends AbstractPlayerCommand {
 
     @Override
     protected void execute(@Nonnull CommandContext commandContext, @Nonnull Store<EntityStore> store, @Nonnull Ref<EntityStore> ref, @Nonnull PlayerRef playerRef, @Nonnull World world) {
-/*        Player player = store.getComponent(ref, Player.getComponentType());
+        Player player = store.getComponent(ref, Player.getComponentType());
         PlayerRef pRef = store.getComponent(ref, PlayerRef.getComponentType());
         if (player == null || pRef == null) {
             return;
-        }*/
+        }
         TriggerBossEvent.dispatch(ref);
-        /*BossHealthHud.setHudManager(player, pRef);
-        HudManager hudManager = player.getHudManager();
-        String bossName = "Golem test name";
 
-        CompletableFuture.runAsync(()-> {
-            if (!(hudManager.getCustomHud() instanceof BossHealthHud)) {
-                hudManager.setCustomHud(playerRef, new BossHealthHud(playerRef, bossName, 1));
-                EventTitleUtil.showEventTitleToPlayer(playerRef,
-                        Message.raw("BOSS FIGHT"), Message.raw(bossName),
-                        true, "ui/icons/skull.png", 3.0f, 0.5f, 0.5f);
+        BlockSelection blockSelection = new BlockSelection();
 
-                // Show notification
-                NotificationUtil.sendNotification(
-                        playerRef.getPacketHandler(),
-                        Message.raw("A powerful enemy approaches!"),
-                        NotificationStyle.Danger);
-            } else {
-                if (hudManager != null) {
-                    hudManager.setCustomHud(playerRef, new EmptyHudUI(playerRef));
-                }
+        TransformComponent transformComponent = store.getComponent(ref, TransformComponent.getComponentType());
+
+        Vector3i playerPos = transformComponent.getPosition().toVector3i();
+
+        int radius = 100;
+
+        Material fromReplace = Material.fromKey("Cloth_Block_Wool_Red");
+        Material toReplace = Material.fromKey("Cloth_Block_Wool_Yellow");
+
+        Vector3i startPosition = playerPos.clone().add(-radius, 0, -radius);
+        Vector3i endPosition = playerPos.clone().add(radius, 0, radius);
+
+        if(fromReplace == null || toReplace == null || player.getReference() == null) {
+            return;
+        }
+
+        for (int y = 50; y < 400; y++) {
+            blockSelection.setSelectionArea(startPosition, endPosition);
+            startPosition.setY(y);
+            endPosition.setY(y);
+            BuilderToolsPlugin.getState(player, pRef).setSelection(blockSelection);
+            BuilderToolsPlugin.getState(player, pRef).replace(player.getReference(), fromReplace, toReplace, store);
+
+            try {
+                TimeUnit.MILLISECONDS.sleep(4);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        }, world);*/
+        }
+
+        BuilderToolsPlugin.getState(player, pRef).setSelection(new BlockSelection());
     }
 }
